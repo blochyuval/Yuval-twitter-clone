9ba134ed7268c1a4a269bd0883568a0c82b4f613
blochyuval <yuvalbloch17@gmail.com>
2025-02-24 11:10:41 +0200

    4- prepare the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
@@ -22,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '5mb'})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); //to pass form data(urlrncoded)
@@ -33,7 +35,15 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
-app.use('/api/notifications', notificationRoutes)
+app.use('/api/notifications', notificationRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  })
+}
 
 
 app.listen(PORT, () => {
