de801e61c8616d7a3f4dd68ac07971aa9b1c9203
blochyuval <yuvalbloch17@gmail.com>
2025-02-24 16:00:58 +0200

    4- changing vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.VITE_API_URL || "http://localhost:8000", // Updated port for Render
         changeOrigin: true,
       },
     },
