611a29d026d0bfd18b3d1996e9b33c5a80c8bd3d
blochyuval <yuvalbloch17@gmail.com>
2025-02-24 15:47:48 +0200

    4- trying more stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
