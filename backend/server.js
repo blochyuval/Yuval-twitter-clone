f891eabf72b31145b37c1c334adfbc7348fa13d3
blochyuval <yuvalbloch17@gmail.com>
2025-02-24 16:26:18 +0200

    more changes due to render

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,12 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:8000", // Updated port for Render
-        changeOrigin: true,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+		},
+	},
 });
